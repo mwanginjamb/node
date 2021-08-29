0ea59c65d8edfa76c46f0bf82fb3549ba425fc2f
mwanginjamb <francnjamb@gmail.com>
2021-08-29 15:30:09 +0300

    Feat: Scaffold all rest api methods in item controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
     return 'Get all items';
+  }
+
+  @Post()
+  create(@Body() item: CreateItemDto): string {
+    return `Name: ${item.name} Desc is ${item.description}`;
+  }
+
+  // Get One Record
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
+  // A more verbose version of passing params
+
+  @Put(':id')
+  update(@Param() param, @Body() Item: CreateItemDto): string {
+    return `Updated record ${param.id}  Whose Name is ${Item.name}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Deleted record: ${id}`;
   }
 }
